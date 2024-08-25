5ece81adecfcb3314cd2f0e5d46a8c4b8519f51b
Hadeermohammed222 <147175593+Hadeermohammed222@users.noreply.github.com>
2024-08-25 08:57:00 -0700

    first commit

--- src/app/Component/admain/admain.component.ts
+++ src/app/Component/admain/admain.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ExaminationService} from '../../services/examination.service';
-import { Validators,FormGroup,FormControl} from '@angular/forms';
+import { ExaminationService } from '../../services/examination.service';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
 import { Question } from 'src/app/models/question.model';
 
 @Component({
@@ -9,102 +9,89 @@ import { Question } from 'src/app/models/question.model';
   styleUrls: ['./admain.component.css']
 })
 export class AdmainComponent implements OnInit {
-  // question:Question;
-  values:any;
-  ques:any;
-  ques_api:any;
-  question:Question= new Question();
-  admainForm: any;
-  myGroup: any;
-  constructor(private examApi :ExaminationService) {}
+  question: Question = new Question();
+  ques: any;
+  ques_api: any = [];
+  myGroup: FormGroup; 
 
+  constructor(private examApi: ExaminationService) {
+    this.myGroup = new FormGroup({});
+  }
+  
   ngOnInit(): void {
-    myGroup: FormGroup;
+    this.myGroup = new FormGroup({
+      question: new FormControl(null, [Validators.required]),
+      type: new FormControl(null, [Validators.required]),
+      option1: new FormControl(null, [Validators.required]),
+      option2: new FormControl(null, [Validators.required]),
+      option3: new FormControl(null, [Validators.required]),
+      answer: new FormControl(null, [Validators.required]),
+    });
+
     this.loadQuestion();
-    this.myGroup   = new FormGroup({
-      question:new FormControl(null,[Validators.required]),
-      type:new FormControl(null,[Validators.required]),
-      option1:new FormControl(null,[Validators.required]),
-      option2:new FormControl(null,[Validators.required]),
-      option3:new FormControl(null,[Validators.required]),
-      answer:new FormControl(null,[Validators.required]),
+  }
+
+  loadQuestion() {
+    this.examApi.getQuestions().subscribe((data) => {
+      this.ques_api = data;
+      // Uncomment the next line if you need to force a full-page reload.
+      // window.location.reload(); // This will reload the entire page
     });
   }
- loadQuestion(){
-  this.examApi.getQuestions().subscribe(
-    data=>{
-      this.ques_api = data;
-    }
-  )
- }
-
- Add(){
-  this.examApi.putQuestions(this.question).subscribe(
-    data =>{
-      console.log(this.question)
+  refresh(){
+    setTimeout(() => {
+      window.location.reload(); // Reload the entire page
+    }, 1000);
+  }
+  Add() {
+    this.examApi.putQuestions(this.question).subscribe(() => {
       this.loadQuestion();
-    }
-  )
+      this.clear();
+      setTimeout(() => {
+        window.location.reload(); // Reload the entire page
+      }, 5000);
+  })
+}
   
- }
- Delete(id_ques: any) {
-  // console.log(id_ques)
-  // console.log(id_ans)
-  if (confirm('Are you sure you want to delete this question?')) 
-  {
-    this.examApi.deleteQuestions(id_ques).subscribe(
-       data =>{
-        this.loadQuestion();
-       }
-    )
-    
- }
-}
-
-Edit(dt:any){
-  this.myGroup.controls['question'].setValue(dt.question);
-  this.myGroup.controls['type'].setValue(dt.type);
-  this.myGroup.controls['option1'].setValue(dt.options[0]);
-  this.myGroup.controls['option2'].setValue(dt.options[1]);
-  this.myGroup.controls['option3'].setValue(dt.options[2]);
-  this.myGroup.controls['answer'].setValue(dt.answer);
-  this.ques = dt.id;
-
-
-}
-clear(){
-  this.myGroup.controls['question'].setValue(' ');
-  this.myGroup.controls['type'].setValue('');
-  this.myGroup.controls['option1'].setValue(' ');
-  this.myGroup.controls['option2'].setValue(' ');
-  this.myGroup.controls['option3'].setValue(' ');
-  this.myGroup.controls['answer'].setValue(' ');
-}
-Update(myGroup: FormGroup) {
-  // Get the form values
-  this.values = this.myGroup.value;
-
-  // Map form values to the question object
-  this.question.question = this.values.question;
-  this.question.type = this.values.type;
-  this.question.options = [
-    this.values.option1,
-    this.values.option2,
-    this.values.option3,
-  ];
-  this.question.answer = this.values.answer;
-  
-  // Update the question
-  this.examApi.updateQuestion(this.ques,this.question).subscribe(
-    data => {
-      // Reload the questions to reflect changes in the browser
-      this.loadQuestion();
+  Delete(id_ques: any) {
+    if (confirm('Are you sure you want to delete this question?')) {
+      this.examApi.deleteQuestions(id_ques).subscribe(() => {
+        this.loadQuestion(); // Reload the list to exclude the deleted question
+      });
     }
-  );
+  }
 
+  Edit(dt: any) {
+    this.myGroup.patchValue({
+      question: dt.question,
+      type: dt.type,
+      option1: dt.options[0],
+      option2: dt.options[1],
+      option3: dt.options[2],
+      answer: dt.answer,
+    });
+    this.ques = dt.id;
+  }
 
-  // Reset the form
-  this.clear();
+  Update() {
+    const values = this.myGroup.value;
+    const updatedQuestion = {
+      ...this.question,
+      question: values.question,
+      type: values.type,
+      options: [values.option1, values.option2, values.option3],
+      answer: values.answer,
+    };
+
+    this.examApi.updateQuestion(this.ques, updatedQuestion).subscribe(() => {
+      this.loadQuestion(); // Reload the list to include the updated question
+      this.clear();
+    });
+  }
+
+  clear() {
+    this.myGroup.reset();
+  }
 }
 
-}
+
--- src/app/services/examination.service.ts
+++ src/app/services/examination.service.ts
@@ -13,22 +13,22 @@ export class ExaminationService {
 
   
   getQuestions(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/questions`);
+    return this.http.get(`${this.baseUrl}`);
   }
   getQuestionById(Id:any):Observable<any>{
-    return this.http.get(`${this.baseUrl}/questions/${Id}`);
+    return this.http.get(`${this.baseUrl}/${Id}`);
   }
   putQuestions(question:any):Observable<any>{
-      return this.http.post(`${this.baseUrl}/questions`,question);
+      return this.http.post(`${this.baseUrl}`,question);
   }   
   updateQuestion(id:any,question:any){
-      return this.http.put(`${this.baseUrl}/questions/${id}`,question);
+      return this.http.put(`${this.baseUrl}/${id}`,question);
   }
   deleteQuestions(id:any):Observable<any>{
-    return this.http.delete(`${this.baseUrl}/questions/${id}`);
+    return this.http.delete(`${this.baseUrl}/${id}`);
   }
   getQuestionsById(id:number):Observable<any>{
-    return this.http.get(`${this.baseUrl}/questions/${id}`);
+    return this.http.get(`${this.baseUrl}/${id}`);
   }
   
 }
